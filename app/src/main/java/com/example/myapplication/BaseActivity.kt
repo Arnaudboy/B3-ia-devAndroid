dca1772b71c55e9675db7054c9eac131af940bcc
Arnaudboy <r.boyer895@laposte.net>
2023-04-11 19:25:41 +0200

    V1 product detail view

--- app/src/main/java/com/example/myapplication/BaseActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
+import android.content.res.Resources.getSystem
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginRight
 
 open class BaseActivity: AppCompatActivity() {
 
@@ -19,8 +22,13 @@ open class BaseActivity: AppCompatActivity() {
     }
 
     fun showBack() {
+        val textView = findViewById<TextView>(R.id.textViewTitle)
         val backLogo=findViewById<ImageView>(R.id.imageViewBack)
         backLogo.visibility= View.VISIBLE
+
+        val layoutParams = textView.layoutParams as RelativeLayout.LayoutParams
+        val px = (60 * getSystem().displayMetrics.density).toInt()
+        layoutParams.setMargins(px, 0, 0, 0)
         backLogo.setOnClickListener(View.OnClickListener {
             this.finish()
         })
--- app/src/main/java/com/example/myapplication/ProductActivity.kt
+++ app/src/main/java/com/example/myapplication/ProductActivity.kt
@@ -1,4 +1,25 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+
 class ProductActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+        val name = intent.getStringExtra("name")
+        val description = intent.getStringExtra("description")
+        val imageUrl = intent.getStringExtra("imageUrl")
+        setViewTitle(name)
+        showBack()
+
+        val image = findViewById<ImageView>(R.id.productDetailImage)
+        Picasso.get().load(imageUrl).into(image)
+
+        val descriptionText = findViewById<TextView>(R.id.productDetailDescription)
+        descriptionText.text = description
+    }
 }
